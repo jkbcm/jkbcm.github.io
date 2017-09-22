ab7de790ca50e65da41d852bccba1915fb656a27
J Brook <jkbcm@users.noreply.github.com>
2017-09-22 17:40:27 +0100

    Add table data and styles

--- testanim.js
+++ testanim.js
@@ -1,14 +1,6 @@
 $(document).ready(function(){
-$( "button" ).on( "click", function() {
-        $( "tbody.subtable" ).animate({
-          backgroundColor: "#00ff00",
-          color: "#fff",
-          width: 100
-        }, 2000 )
-    });
-$( '#table_btn').on('click', function() {
-    $('table').toggleClass('before, after');
-});
-
+	$('#table_btn').on('click', function() {
+      $('table').toggleClass('before, after');
+  });
 });
    
